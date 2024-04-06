cd13a4f3f0f0f6e6ba6aef19cab2700145ea27e9
DanielaSGomez <danielagomez992@hotmail.com>
2024-04-06 08:51:22 -0300

    Base diseño

---
+++ script.js
@@ -0,0 +1,5 @@
+const displayPrimerNumero = document.getElementById('primerNumero');
+const displaySegundoNumero = document.getElementById('segundoNumero');
+
+const numeros  = document.querySelectorAll('.numerito');
+const operadores = document.querySelectorAll('.smoothOperator');
